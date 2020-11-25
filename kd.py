7ec97571b212124412ab0670e6d39d6ba01e120e
Turing <turingksc@gmail.com>
2020-11-25 17:29:24 +0800

    .

--- kd.py
+++ kd.py
@@ -26,10 +26,10 @@ teacher_optimizer = optim.SGD(teacher_model.fc3_256_1.parameters(), lr=0.01)
 student_optimizer = optim.SGD(student_model.parameters(), lr=0.01)
 
 
-train_loader = torch.utils.data.DataLoader(DataLmdb("D:\\_DNN_Opt\\LowDB\\Low_lmdb\\Train-Low_lmdb", db_size=143432, crop_size=128, flip=True, scale=0.00390625),
-    batch_size=32, shuffle=True)
-test_loader = torch.utils.data.DataLoader(DataLmdb("D:\\_DNN_Opt\\LowDB\\Low_lmdb\\Valid-Low_lmdb", db_size=7939, crop_size=128, flip=False, scale=0.00390625, random=False),
-    batch_size=32, shuffle=False)
+train_loader = torch.utils.data.DataLoader(DataLmdb("/kaggle/working/Low_Test/Train-Low_lmdb", db_size=143432, crop_size=128, flip=True, scale=0.00390625),
+    batch_size=128, shuffle=True)
+test_loader = torch.utils.data.DataLoader(DataLmdb("/kaggle/working/Low_Test/Valid-Low_lmdb", db_size=7939, crop_size=128, flip=False, scale=0.00390625, random=False),
+    batch_size=128, shuffle=False)
 
 # Train using KD_Lib
 
